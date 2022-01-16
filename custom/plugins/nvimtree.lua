cb0559619d7edb44e516e39121d05994a3fd0887
ericwq <ericwq057@qq.com>
2022-01-16 22:27:45 +0800

    g.nvim_tree_quit_on_open = 0

--- custom/plugins/nvimtree.lua
+++ custom/plugins/nvimtree.lua
@@ -6,7 +6,7 @@ local git_status = conf.enable_git
 local ui = conf.ui
 
 if not present then
-   return
+	return
 end
 
 local g = vim.g
@@ -15,59 +15,71 @@ g.nvim_tree_add_trailing = 0 -- append a trailing slash to folder names
 g.nvim_tree_git_hl = git_status
 g.nvim_tree_highlight_opened_files = 0
 g.nvim_tree_indent_markers = 1
-g.nvim_tree_quit_on_open = 1 -- closes tree when file's opened
-g.nvim_tree_root_folder_modifier = table.concat { ":t:gs?$?/..", string.rep(" ", 1000), "?:gs?^??" }
+g.nvim_tree_quit_on_open = 0 -- closes tree when file's opened
+g.nvim_tree_root_folder_modifier = table.concat {
+	":t:gs?$?/..",
+	string.rep(" ", 1000),
+	"?:gs?^??",
+}
 
 g.nvim_tree_window_picker_exclude = {
-   filetype = { "notify", "packer", "qf" },
-   buftype = { "terminal" },
+	filetype = {
+		"notify",
+		"packer",
+		"qf",
+	},
+	buftype = {
+		"terminal",
+	},
 }
 
 g.nvim_tree_show_icons = {
-   folders = 1,
-   files = 1,
-   git = git_status,
+	folders = 1,
+	files = 1,
+	git = git_status,
 }
 
 g.nvim_tree_icons = {
-   default = "",
-   symlink = "",
-   git = {
-      deleted = "",
-      ignored = "◌",
-      renamed = "➜",
-      staged = "✓",
-      unmerged = "",
-      unstaged = "✗",
-      untracked = "★",
-   },
-   folder = {
-      default = "",
-      empty = "",
-      empty_open = "",
-      open = "",
-      symlink = "",
-      symlink_open = "",
-   },
+	default = "",
+	symlink = "",
+	git = {
+		deleted = "",
+		ignored = "◌",
+		renamed = "➜",
+		staged = "✓",
+		unmerged = "",
+		unstaged = "✗",
+		untracked = "★",
+	},
+	folder = {
+		default = "",
+		empty = "",
+		empty_open = "",
+		open = "",
+		symlink = "",
+		symlink_open = "",
+	},
 }
 
 nvimtree.setup {
-   filters = {
-      dotfiles = false,
-   },
-   disable_netrw = true,
-   hijack_netrw = true,
-   ignore_ft_on_setup = { "dashboard" },
-   auto_close = true,
-   open_on_tab = false,
-   hijack_cursor = true,
-   update_cwd = true,
-   update_focused_file = {
-      enable = true,
-      update_cwd = false,
-   },
-   view = ui,
-   git = {
-      ignore = false,
-   },
+	filters = {
+		dotfiles = false,
+	},
+	disable_netrw = true,
+	hijack_netrw = true,
+	ignore_ft_on_setup = {
+		"dashboard",
+	},
+	auto_close = true,
+	open_on_tab = false,
+	hijack_cursor = true,
+	update_cwd = true,
+	update_focused_file = {
+		enable = true,
+		update_cwd = false,
+	},
+	view = ui,
+	git = {
+		ignore = false,
+	},
 }
