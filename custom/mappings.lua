2feee1f8720af77f5d3e38667114ef88a59b76cf
Qi Wang <ericwq057@qq.com>
2022-01-26 14:59:22 +0800

    change SymbolsOutline map key to <leader>j

--- custom/mappings.lua
+++ custom/mappings.lua
@@ -3,7 +3,7 @@ local map = require("core.utils").map
 -- map('n', '<C-m>', ':Vista!!<CR>', opt) -- open/close
 map("n", "<leader>fs", ":Telescope grep_string<CR>")
 map("n", "<leader>ft", ":Telescope treesitter<CR>")
-map("n", "<leader>t", ":SymbolsOutline<CR>")
+map("n", "<leader>j", ":SymbolsOutline<CR>")
 -- NOTE: the 4th argument in the map function can be a table i.e options but its most likely un-needed so dont worry about it
 
 --[[
