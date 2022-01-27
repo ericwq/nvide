9792d8e6108802081262c0ac40b2af7a642de4b1
Qi Wang <ericwq057@qq.com>
2022-01-27 18:43:03 +0800

    enhance treesitter with indent and incremental_selection

--- custom/plugins/configs.lua
+++ custom/plugins/configs.lua
@@ -13,6 +13,24 @@ M.treesitter = {
 	},
 	highlight = {
 		enable = true,
+		use_languagetree = true,
+		additional_vim_regex_highlighting = false,
+	},
+
+	-- enable Indentation
+	indent = {
+		enable = true,
+	},
+
+	-- enable Incremental selection
+	incremental_selection = {
+		enable = true,
+		keymaps = {
+			init_selection = "gnn",
+			node_incremental = "grn",
+			scope_incremental = "grc",
+			node_decremental = "grm",
+		},
 	},
 }
 
