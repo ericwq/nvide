dcc6d711909fd84887c48b7d992dbf13f022dab0
Qi Wang <ericwq057@qq.com>
2023-03-24 09:41:57 +0800

    - refine chadrc.lua according to NvChad 2.0. - add comments for mappings.lua.

--- custom/chadrc.lua
+++ custom/chadrc.lua
@@ -1,23 +1,33 @@
--- This is the chadrc file , its supposed to be placed in /lua/custom dir
--- lua/custom/chadrc.lua
--- remove this if you dont use custom.init.lua at all
+-- @type ChadrcConfig
 local M = {}
 
--- make sure you maintain the structure of `core/default_config.lua` here,
--- M.plugins = "custom.plugins"
+-- Path to overriding theme and highlights files
+local highlights = require "custom.highlights"
 
 -- changing theme and UI
 M.ui = {
 	theme = "chadracula",
-	hl_override = {
-		Comment = {
-			fg = "#808080",
-			italic = true,
-		},
+	theme_toggle = {
+		"chadracula",
+		"onedark",
+		"one_light",
 	},
+
+	hl_override = highlights.override,
+	hl_add = highlights.add,
+
+	-- hl_override = {
+	-- 	Comment = {
+	-- 		fg = "#808080",
+	-- 		italic = true,
+	-- 	},
+	-- },
 	transparency = true,
 }
 
+-- M.plugins = "custom.plugins"
+
+-- check core.mappings for table structure
 M.mappings = require "custom.mappings"
 
 return M
