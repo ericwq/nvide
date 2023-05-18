7d28733836e87679b53d65730a83f18c132faf90
Qi Wang <ericwq057@qq.com>
2023-05-18 12:44:08 +0800

    - disable redundent auto command for yank.

--- custom/configs/osc52.lua
+++ custom/configs/osc52.lua
@@ -2,15 +2,15 @@
 --
 require("osc52").setup()
 
-local function copy2()
-	if vim.v.event.operator == 'y' and vim.v.event.regname == '+' then
-		require('osc52').copy_register('+')
-	end
-end
-
-vim.api.nvim_create_autocmd('TextYankPost', {
-	callback = copy2,
-})
+-- local function copy2()
+-- 	if vim.v.event.operator == 'y' and vim.v.event.regname == '+' then
+-- 		require('osc52').copy_register('+')
+-- 	end
+-- end
+--
+-- vim.api.nvim_create_autocmd('TextYankPost', {
+-- 	callback = copy2,
+-- })
 
 local function copy(lines, _)
 	require('osc52').copy(table.concat(lines, '\n'))
