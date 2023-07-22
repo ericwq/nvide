357ebce6e0a0dc5b501501f826b1b58c26e437b8
ericwq <ericwq057@qq.com>
2023-07-22 17:51:48 +0800

    - roll back to the individual language server setup.

--- custom/configs/lspconfig.lua
+++ custom/configs/lspconfig.lua
@@ -1,16 +1,102 @@
-local on_attach = require("plugins.configs.lspconfig").on_attach
+-- local on_attach = require("plugins.configs.lspconfig").on_attach
+local attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = {
-	"dockerls",
-	"gopls",
-	"clangd",
+-- local servers = {
+-- 	"dockerls",
+-- 	"gopls",
+-- 	"clangd",
+-- }
+--
+-- for _, lsp in ipairs(servers) do
+-- 	lspconfig[lsp].setup {
+-- 		on_attach = on_attach,
+-- 		capabilities = capabilities,
+-- 	}
+-- end
+
+lspconfig.dockerls.setup {
+	on_attach = function(client, bufnr)
+		attach(client, bufnr)
+		client.server_capabilities.documentFormattingProvider = true
+		client.server_capabilities.documentRangeFormattingProvider = true
+	end,
+	-- on_attach = on_attach_vim,
+	capabilities = capabilities,
+	filetypes = {
+		"dockerfile",
+		"Dockerfile",
+	},
+};
+
+-- c/c++ language server
+lspconfig.clangd.setup {
+	-- on_attach = attach,
+	on_attach = function(client, bufnr)
+		attach(client, bufnr)
+		client.server_capabilities.documentFormattingProvider = true
+		client.server_capabilities.documentRangeFormattingProvider = true
+	end,
+	capabilities = capabilities,
+	root_dir = lspconfig.util.root_pattern("compile_commands.json", "compile_flags.txt", ".git"),
+	flags = {
+		debounce_text_changes = 150,
+	},
+	cmd = {
+		"clangd",
+		"--background-index",
+		"--clang-tidy",
+	},
+	filetypes = {
+		"c",
+		"cpp",
+		"objc",
+		"objcpp",
+	},
 }
 
-for _, lsp in ipairs(servers) do
-	lspconfig[lsp].setup {
-		on_attach = on_attach,
-		capabilities = capabilities,
-	}
-end
+-- go language server
+-- cmd = {"gopls", "-remote", "auto", "-logfile", "/tmp/gopls.log"},
+lspconfig.gopls.setup {
+	cmd = {
+		"gopls",
+		"serve",
+		-- "-rpc.trace",
+		-- "-logfile=auto",
+	},
+	flags = {
+		debounce_text_changes = 150,
+	},
+	on_attach = function(client, bufnr)
+		attach(client, bufnr)
+		client.server_capabilities.documentFormattingProvider = true
+		client.server_capabilities.documentRangeFormattingProvider = true
+	end,
+	-- on_attach = on_attach_vim,
+	capabilities = capabilities,
+	settings = {
+		gopls = {
+			-- https://github.com/golang/tools/blob/master/gopls/doc/settings.md#allowimplicitnetworkaccess-bool
+			-- allowImplicitNetworkAccess disables GOPROXY=off, allowing implicit module downloads rather
+			-- than requiring user action. This option will eventually be removed.
+			allowImplicitNetworkAccess = true,
+			-- experimentalWorkspaceModule = true,
+			-- analyses = {
+			-- 	unusedparams = true,
+			-- },
+			-- staticcheck = true,
+			linksInHover = false,
+			-- codelenses = {
+			-- 	generate = true,
+			-- 	gc_details = true,
+			-- 	regenerate_cgo = true,
+			-- 	tidy = true,
+			-- 	upgrade_depdendency = true,
+			-- 	vendor = true,
+			-- },
+			usePlaceholders = true,
+			gofumpt = true,
+		},
+	},
+}
