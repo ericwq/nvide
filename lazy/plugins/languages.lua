683b85f43edfa3c83499722a51b1cf1b1228cb37
Wang Qi <ericwq057@qq.com>
2024-06-21 22:59:12 +0800

    replace with mini-comment.

--- lazy/plugins/languages.lua
+++ lazy/plugins/languages.lua
@@ -1,19 +1,10 @@
 return {
-  {
-    import = "lazyvim.plugins.extras.lang.go",
-  },
-  {
-    import = "lazyvim.plugins.extras.lang.markdown",
-  },
-  {
-    import = "lazyvim.plugins.extras.lang.clangd",
-  },
-  {
-    import = "lazyvim.plugins.extras.editor.aerial",
-  },
-  {
-    import = "lazyvim.plugins.extras.ui.treesitter-context",
-  },
+  -- { import = "lazyvim.plugins.extras.lang.markdown", },
+  { import = "lazyvim.plugins.extras.lang.go" },
+  { import = "lazyvim.plugins.extras.lang.clangd" },
+  { import = "lazyvim.plugins.extras.editor.aerial" },
+  { import = "lazyvim.plugins.extras.ui.treesitter-context" },
+  { import = "lazyvim.plugins.extras.coding.mini-comment" },
   {
     "neovim/nvim-lspconfig",
     opts = {
