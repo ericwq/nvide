b3acd59ab091b2a619a01af92cb174021c05e827
Qi Wang <ericwq057@qq.com>
2024-06-21 14:38:12 +0800

    fix init order problem for clangd.lua.

--- lazy/plugins/languages.lua
+++ lazy/plugins/languages.lua
@@ -1,14 +1,24 @@
 return {
-	{
-		import = "lazyvim.plugins.extras.lang.go",
-	},
-	-- {
-	-- 	import = "lazyvim.plugins.extras.lang.clangd",
-	-- },
-	{
-		import = "lazyvim.plugins.extras.editor.aerial",
-	},
-	{
-		import = "lazyvim.plugins.extras.ui.treesitter-context",
-	},
+  {
+    import = "lazyvim.plugins.extras.lang.go",
+  },
+  -- {
+  --   import = "lazyvim.plugins.extras.lang.clangd",
+  -- },
+  {
+    import = "lazyvim.plugins.extras.editor.aerial",
+  },
+  {
+    import = "lazyvim.plugins.extras.ui.treesitter-context",
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        clangd = {
+          mason = false,
+        },
+      },
+    },
+  },
 }
