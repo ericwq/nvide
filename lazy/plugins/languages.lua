0ddd8d20587937fb0f598993e5a81155adbeae52
Wang Qi <ericwq057@qq.com>
2024-06-25 06:59:12 +0800

    lazy plugins: disable extra java lang. disable openjdk, jdtls.

--- lazy/plugins/languages.lua
+++ lazy/plugins/languages.lua
@@ -1,9 +1,9 @@
 return {
   -- { import = "lazyvim.plugins.extras.lang.markdown", },
+  -- { import = "lazyvim.plugins.extras.lang.java" },
   { import = "lazyvim.plugins.extras.lang.docker" },
   { import = "lazyvim.plugins.extras.lang.go" },
   { import = "lazyvim.plugins.extras.lang.clangd" },
-  { import = "lazyvim.plugins.extras.lang.java" },
   { import = "lazyvim.plugins.extras.editor.outline" },
   { import = "lazyvim.plugins.extras.ui.treesitter-context" },
   { import = "lazyvim.plugins.extras.coding.mini-comment" },
